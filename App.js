4155dd45b7a9aa6bec5ddf4518bf9352f7c2ba5d
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-06-03 13:06:34 +0530

    expense task21 Fixing the Cart problems

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Fragment, useEffect } from 'react';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector,useDispatch } from 'react-redux';
-import { uiActions } from './store/ui-slice';
+import { sendCartData,fetchCartData } from './store/Cart-actions';
 import Notification from './components/UI/Notification';
 
 
@@ -15,51 +15,22 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://expense-auth-ce2b3-default-rtdb.firebaseio.com//cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
+ 
 
   return (
     <Fragment>
